ada175fd36bd107052fd60192029ebe3d9ff3378
sergei_arkhipov_1 <byrgyin@yandex.ru>
2025-08-29 16:17:44 +0300

    begin_transfer_monye

--- frontend/src/views/AccountsInnerView.vue
+++ frontend/src/views/AccountsInnerView.vue
@@ -3,16 +3,23 @@ import {computed, ref} from 'vue'
 import {useRoute} from "vue-router";
 import {formatNumber, getLocalStorageToken, ParseShortDotDate} from "@/composables/helpers.ts";
 import {loadAccount} from "@/api/apiFetch.ts";
+import {useListAccountStore} from "@/stores/store.ts";
 import type {Account} from "@/types/types.ts";
 
 const transHistoryArr = ref<string[]>(['Счёт отправителя', 'Счёт получателя', 'Сумма', 'Дата']);
 const account = useRoute().params.account as string;
 const token = getLocalStorageToken();
 const dataPage = ref<Account | null>(null);
+const accountTo = ref<string>('');
+const howMuch = ref<number | null>(null);
 
-let formatMoney = computed(() => {
+const formatMoney = computed(() => {
   return formatNumber(dataPage?.value?.balance);
-})
+});
+
+const filteredSelectAccount = computed(() => {
+  return useListAccountStore().listAccounts.filter(item => item.account !== dataPage.value?.account)
+});
 
 const loadPage = async () => {
   dataPage.value = await (await loadAccount(token, account)).payload;
@@ -39,15 +46,15 @@ loadPage();
           <form class="accounts-inner__middle-form">
             <label class="accounts-inner__middle-form-label" for="account">
               <span>Номер счёта</span>
-              <select class="accounts-inner__middle-form-input" id="account" required name="account">
-                <option value="74213041477477406320783754">74213041477477406320783754</option>
+              <select v-model="accountTo" class="accounts-inner__middle-form-input" id="account" required name="account">
+                <option v-for="item in filteredSelectAccount" :value="item.account">{{item.account}}</option>
               </select>
             </label>
             <label class="accounts-inner__middle-form-label" for="amount">
               <span>Сумма перевода</span>
-              <input class="accounts-inner__middle-form-input" id="amount" type="number" placeholder="Placeholder" required name="amount">
+              <input v-model="howMuch" class="accounts-inner__middle-form-input" id="amount" min="0" type="number" placeholder="Placeholder" required name="amount">
             </label>
-            <button class="accounts-inner__middle-form-inputsubmit" type="submit">Отправить</button>
+            <button class="button accounts-inner__middle-form-inputsubmit" type="submit">Отправить</button>
           </form>
         </div>
       </div>
@@ -198,5 +205,52 @@ loadPage();
 
 .accounts-inner__list-item--rate-minus {
   color: #FD4E5D;
+}
+.accounts-inner__middle-wrap-form {
+  padding: 25px 42px;
+  width: 100%;
+  max-width: 581px;
+  background: #F3F4F6;
+  border-radius: 40px;
+}
+.accounts-inner__middle-title {
+  margin-bottom: 25px;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 23px;
+  letter-spacing: -0.02em;
+}
+.accounts-inner__middle-form{
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 25px;
+  justify-content: flex-end;
+}
+.accounts-inner__middle-form-label {
+  display: flex;
+  gap: 18px;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 15px;
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: -0.01em;
+}
+.accounts-inner__middle-form-inputsubmit{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  width: fit-content;
+  text-align: center;
+}
+.accounts-inner__middle-form-input {
+  padding: 10px 16px;
+  border: 1px solid #9CA3AF;
+  border-radius: 7px;
+  width: 100%;
+  height: 44px;
+  max-width: 300px;
 }
 </style>
